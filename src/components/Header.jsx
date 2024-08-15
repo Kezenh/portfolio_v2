1273f7e710da57b1187379c540175d3574c12264
Kezenh <quentin425@hotmail.fr>
2024-08-15 16:12:51 +0200

    Implementation of night mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+
 function Header() {
+
+    const [nightTheme, setNightTheme] = useState(true);
+
+    function changeTheme() {
+        const root = document.documentElement;
+        if (nightTheme) {
+            root.style.setProperty('--first-color', 'var(--light-color)');
+            root.style.setProperty('--second-color', 'var(--dark-color)');
+        } else {
+            root.style.setProperty('--first-color', 'var(--dark-color)');
+            root.style.setProperty('--second-color', 'var(--light-color)');
+        }
+        setNightTheme(!nightTheme);
+    }
+
     return (
-        <p>Salut, c'est la v1 de mon portfolio</p>
+        <header>
+            <p>Salut, c'est la v1 de mon portfolio</p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <button onClick={changeTheme}>{nightTheme ? <p>Jour</p> : <p>Nuit</p>}</button>
+        </header>
     )
 }
 
